4118b612140a381cb323c62e9317ebbf7d6db30b
iwag <iwagame@gmail.com>
2014-12-30 22:12:55 +0900

    initial

---
+++ seach-nicovideo-rb.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |spec|
+  spec.name          = "search-nicovideo-rb"
+  spec.version       = "0.1.0"
+  spec.authors       = ["iwag"]
+  spec.email         = ["iwagame@gmail.com"]
+  spec.summary       = "http://search.nidovideo.jp/ 's query builder and so."
+  spec.files         = ["search.rb"]
+  spec.require_path  = "."
+end
+
---
+++ search.rb
@@ -0,0 +1,141 @@
+require 'net/http'
+require 'json'
+
+module SearchNicovideo
+
+  def uri_string
+    'http://search.nicovideo.jp/api/'
+  end
+
+  def uri
+    URI.parse(uri_string)
+  end
+
+  def search_raw(q)
+    res = nil
+    uri = uri()
+    Net::HTTP.start(uri.host, uri.port){|http|
+      res = http.post(uri.path, q)
+    }
+    res
+  end
+
+  def search(q) # q must be JSON
+    res = search_raw(q)
+    arr = res.body.split("\n").map{|i| JSON.parse(i) }
+    hits = arr.select{ |i| i["type"]=="hits" && ! i["endofstream"] }
+    stats = arr.select{ |i| i["type"]=="stat" && ! i["endofstream"] }
+    {
+      stats: stats,
+      hits: hits
+    }
+  end
+
+  class QueryBuilder
+    def  initialize()
+      @q = {query: "test", service: ["video"], search: ["title", "description"], join: ["title"], issuer: "http://github.com/iwag/search-nicovideo-rb", reason:"ma10"}
+    end
+
+    def query(s)
+      @q[:query] = s
+      self
+    end
+
+    def service(s)
+      if s.is_a?(Array)
+        @q[:service] = s
+      end
+      self
+    end
+
+    def search(s)
+      if s.is_a?(Array)
+        @q[:search] = s
+      end
+      self
+    end
+
+    def join(j)
+      if j.is_a?(Array)
+        @q[:join] = j
+      end
+      self
+    end
+
+    def issuer(s)
+      if s.is_a?(String)
+        @q[:issuer] = s
+      end
+      self
+    end
+
+    def sort_by(s)
+      if s.is_a?(String)
+        @q[:sort_by] = s
+      end
+      self
+    end
+
+    def desc(d)
+      if d==true
+        @q[:order] = "desc"
+      else
+        @q[:order] = "asc"
+      end
+      self
+    end
+
+    def filters(f)
+      @q[:filters] = f
+      self
+    end
+
+    def build
+      @q
+    end
+
+    def to_json
+      @q.to_json
+    end
+  end
+
+  class FilterBuilder
+    def initialize()
+      @f = { type: "equal", field: "view_counter", value: "10" }
+    end
+
+    def type(t)
+      @f[:type] = t
+      self
+    end
+
+    def field(f)
+      @f[:f] = f
+      self
+    end
+
+    def value(v)
+      @f[:v] = v
+      self
+    end
+
+    def from(v) 
+      @f[:from] = v
+      self
+    end
+
+    def to(v)
+      @f[:to] = v
+      self
+    end
+      
+    def build
+      @f
+    end
+
+    def to_json
+      @f.to_json
+    end
+  end
+
+end 
